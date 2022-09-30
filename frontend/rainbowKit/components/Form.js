f3e3b0b173fd1849021c676e7dd2275f01cb8a7d
m <13090422+marciob@users.noreply.github.com>
2022-09-30 14:05:20 -0300

    cleaning a bit

--- frontend/rainbowKit/components/Form.js
+++ frontend/rainbowKit/components/Form.js
@@ -8,9 +8,9 @@ import { ethers } from "ethers";
 function Form() {
   const { register, handleSubmit, errors, reset } = useForm();
 
-  const { address } = useAccount();
+  let { address } = useAccount();
 
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState(0.001);
   const [uri, setUri] = useState("");
 
   function onSubmitForm(values) {
@@ -24,12 +24,11 @@ function Form() {
     addressOrName: "0xcf5c9BAb07D7F63847348F4B4e06F2e7bdcAB240",
     contractInterface: abi,
     functionName: "safeMint",
-    args: ["0x95a548A77f41d64f5F0d6905f8F9CD3aeFe972A9", uri],
+    args: ["0x95a548A77f41d64f5F0d6905f8F9CD3aeFe972A9", uri], //change it later, not hard coded address
     chainId: 5,
     overrides: {
-      from: "0x95a548A77f41d64f5F0d6905f8F9CD3aeFe972A9",
-      value: ethers.utils.parseEther("0.01"),
-      // value: ethers.utils.parseEther(price),
+      from: "0x95a548A77f41d64f5F0d6905f8F9CD3aeFe972A9", //change it later, not hard coded address
+      value: ethers.utils.parseEther(price.toString()),
     },
   });
 
@@ -102,80 +101,22 @@ function Form() {
                 {/* button field */}
                 <div className="p-2 w-full">
                   <button
-                    onClick={() => safeMint?.()}
+                    onClick={() => {
+                      console.log(
+                        "onclick safemint clicked. current state price :",
+                        price
+                      );
+                      var value = document.getElementsByName("price")[0].value;
+                      console.log("validation ", value);
+                      safeMint?.();
+                    }}
                     className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none border-blue-600 rounded text-lg"
                   >
                     Mint
                   </button>
                 </div>
                 {/* footer field */}
-                {/* <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <a className="text-indigo-500">example@email.com</a>
-                <p className="leading-normal my-5">
-                  49 Smith St.
-                  <br />
-                  Saint Cloud, MN 56301
-                </p>
-                <span className="inline-flex">
-                  <a className="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <rect
-                        width="20"
-                        height="20"
-                        x="2"
-                        y="2"
-                        rx="5"
-                        ry="5"
-                      ></rect>
-                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div> */}
+                {/* <TheFoot /> */}
               </div>
             </form>
           </div>
