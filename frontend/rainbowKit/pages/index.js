b503912d8b1292cc951f7714cd20696a020d6440
m <13090422+marciob@users.noreply.github.com>
2022-09-29 21:46:39 -0300

    remove info component from index.js

--- frontend/rainbowKit/pages/index.js
+++ frontend/rainbowKit/pages/index.js
@@ -1,12 +1,10 @@
 import Navbar from "../components/Navbar";
-import Info from "../components/Info";
 import Form from "../components/Form";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Info />
       <Form />
     </div>
   );
